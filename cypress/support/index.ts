dbe233237deeb3c1eee12a1ef12c2df7e1a09934
quy.pham <Quy.Pham@rci.rogers.com>
2023-02-07 09:42:17 +0700

    Remove unnessarry login action at places - Put tsconfig.json at where ts files locates inside to prevent IDE error

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -16,14 +16,3 @@ declare global {
 //         cy.get(el.loginBtn).should('be.visible').click()
 //     })
 // })
-
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
--- cypress/support/login.actions.ts
+++ cypress/support/login.actions.ts
@@ -1,8 +1,8 @@
-Cypress.Commands.add('login', (userName, password) => {
-    cy.fixture('example').then((el)=>{
+// Cypress.Commands.add('login', (userName, password) => {
+//     cy.fixture('example').then((el)=>{
 
-        cy.get(el.usernameField).should('be.visible').type(userName)
-        cy.get(el.passwordField).should('be.visible').type(password)
-        cy.get(el.loginBtn).should('be.visible').click()
-    })
-})
+//         cy.get(el.usernameField).should('be.visible').type(userName)
+//         cy.get(el.passwordField).should('be.visible').type(password)
+//         cy.get(el.loginBtn).should('be.visible').click()
+//     })
+// })
